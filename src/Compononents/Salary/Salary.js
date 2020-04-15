48d3c47b19614e25fe2e80448e7a488541ef8671
tanmoytonu <tanmoyacharjee18@gmail.com>
2020-04-16 03:24:36 +0600

    4th project done

--- src/Compononents/Salary/Salary.js
+++ src/Compononents/Salary/Salary.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Salary = (props) => {
     const salary=props.salary
-    // const total = salary.reduce((total,usr) => total + usr.salary,0);
+    
     let total =0;
     for(let i=0; i<salary.length; i++){
         const user = salary[i];
